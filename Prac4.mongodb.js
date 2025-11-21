4806acd14ca7c298784a8d0aeae79e0ab02fba0f
ramirezarteagadulce46-boop <ramirez.arteaga.dulce.46@cbtis118.edu.mx>
2025-11-21 16:38:12 +0000

    l

--- Prac4.mongodb.js
+++ Prac4.mongodb.js
@@ -1,1 +1,15 @@
 
+db.aves.insertOne({nombre:  "Pájaro carpintero"})
+db.aves.insertOne({habitat: "Bosques"})
+
+db.reptiles.insertOne({nombre: "Cocodrilo"})
+db.reptiles.insertOne({habitat: "Ríos de agua dulce"})
+
+db.peces.insertOne({nombre: "Salmon"})
+db.peces.insertOne({habitat: "Aguas dulces y salada"})
+
+db.mamiferos.insertOne({nombre: "Elefante"})
+db.mamiferos.insertOne({habitat: "Sabanas de África"})
+
+db.anfibios.insertOne({ nombre: "Salamandra de fuego"})
+db.anfibios.insertOne({ habitat: "Arroyos y estanques"})
